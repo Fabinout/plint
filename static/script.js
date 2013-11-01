2c8179a865fe6763d36deef39203b9508e2e891e
Antoine Amarilli <a3nm@a3nm.net>
2013-11-01 15:39:01 +0100

    move script to own file

---
+++ static/script.js
@@ -0,0 +1,7 @@
+function showCustom(a) {
+  if (a) {
+    document.getElementById("custom_template").style.display = "block";
+  } else {
+    document.getElementById("custom_template").style.display = "none";
+  }
+}
