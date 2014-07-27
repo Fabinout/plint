67c93dd8b5710b6afacfbdf581cbbbd5c156c4fe
Antoine Amarilli <a3nm@a3nm.net>
2014-07-27 11:40:06 +0200

    train diaeresis file by name

--- onepass.sh
+++ onepass.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# read current diaeresis file on stdin and take unique prefix as $1
+# read current diaeresis file as $2 and take unique prefix as $1
 # produce on stdout new diaeresis file
 
 cd "$( dirname "$0" )"
 NUM="$1"
+DFILE="$2"
+shift
 shift
 
-TEMP=`mktemp`
 mkdir -p contexts
-mv -f diaeresis.json $TEMP
-cat > diaeresis.json
 for f in "$@"
 do
   DEST=$(echo "contexts/$f.$NUM" | sed 's!/\.\.!!g')
-  ./plint.py test/$f.tpl "$DEST" < test/$f
+  ./plint.py test/$f.tpl $DFILE "$DEST" < test/$f
 done
-mv $TEMP diaeresis.json
 cat contexts/* | ./haspirater/buildtrie_list.py |
   ./haspirater/compresstrie.py
 
