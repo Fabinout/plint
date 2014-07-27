f5bfaa1b3f39db027387b09048c603463ce1a09d
Antoine Amarilli <a3nm@a3nm.net>
2014-07-27 11:40:22 +0200

    diaeresis_cyrano file

--- test.sh
+++ test.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
 echo "It is normal that some errors occur when running this script"
-for a in test/*.tpl; do echo "$a"; ./plint.py $a < ${a%.tpl}; done
+for a in test/*.tpl; do
+  echo "$a"
+  if [[ $a == *cyrano_full* ]]
+  then
+    ./plint.py $a diaeresis_cyrano.json < ${a%.tpl}
+  else
+    ./plint.py $a < ${a%.tpl}
+  fi
+done
