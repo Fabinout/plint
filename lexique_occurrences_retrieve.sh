a0d9c34e6f0791304345d22ccf86e9377b8c5b31
Antoine Amarilli <a3nm@a3nm.net>
2015-01-25 18:24:50 +0100

    additions_occurrences

--- lexique_occurrences_retrieve.sh
+++ lexique_occurrences_retrieve.sh
@@ -9,6 +9,8 @@ cd "$( dirname "$0" )"
 wget $URL
 unzip -qq $ZIP $FILE
 cat $FILE | ./lexique_fix.sh | cut -f1 |
-  rev | cut -d' ' -f1 | rev | sort | uniq -c |
+  rev | cut -d' ' -f1 | rev |
+  cat - additions_occurrences |
+  sort | uniq -c |
   awk '{print $2, $1}'
 
