f4498ba9a0dd2540b022274f7850fe011ea26bf6
Antoine Amarilli <a3nm@a3nm.net>
2019-08-11 21:23:40 +0200

    document normal test output

--- test.sh
+++ test.sh
@@ -3,7 +3,8 @@
 # about the hiatus error found in malherbe, see:
 # http://books.google.de/books?id=qpQGAAAAQAAJ&pg=PA40&lpg=PA40&dq=Et+que+pour+retarder+une+heure+seulement+%22Pour+ne+mourir+jamais%22,+meure+%C3%A9ternellement&source=bl&ots=G8VEFnXkmB&sig=qzmHRiloQpIp6Ebb-9aJrYOoIM0&hl=en&sa=X&ei=3oL8T9WiJ6aB4gTK8pWPBw&redir_esc=y#v=onepage&q=Et%20que%20pour%20retarder%20une%20heure%20seulement%20%22Pour%20ne%20mourir%20jamais%22%2C%20meure%20%C3%A9ternellement&f=false
 
-echo "It is normal that some errors occur when running this script"
+echo "It is normal that some errors occur when running this script" >/dev/stderr
+echo "See test_expected_output.out for the usual errors that are output" >/dev/stderr
 for a in test/*.tpl; do
   echo "$a"
   if [[ $a == *cyrano_full* ]]
